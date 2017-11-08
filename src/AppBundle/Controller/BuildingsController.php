9a51c78e6d8b871391a68d279a28a526670d2747
sammar.javed <s.javed@ebz-bs.de>
2017-11-08 04:20:00 +0100

    Controller method to insert the received data into database using post request

--- src/AppBundle/Controller/BuildingsController.php
+++ src/AppBundle/Controller/BuildingsController.php
@@ -15,7 +15,32 @@ use Doctrine\ORM\EntityRepository;
  */
 class BuildingsController extends Controller
 {
+    /**
+     * @Route("/insert")
+     * @Method("POST")
+     */
+    public function insertAction(Request $request)
+    {
+        $success=true;
+        header("Access-Control-Allow-Origin: *");
+        $data = json_decode($request->getContent(), true);
+        $em = $this->getDoctrine()->getEntityManager();
+        $building = new Buildings();
+        $building->setMoveindate($data['moveindate']);
+        $building->setStreet($data['street']);
+        $building->setPostcode($data['postcode']);
+        $building->setTown($data['town']);
+        $building->setCountry($data['country']);
+        $building->setEmail($data['emailGroup']['email']);
 
+
+        $em->persist($building);
+        $em->flush();
+
+        return new JsonResponse('success : '.$success);
+
+
+    }
     /**
      * @Route("/list")
      */
